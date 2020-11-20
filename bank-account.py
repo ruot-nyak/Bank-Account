c61ef6d3258007888a1a5418800153195d560d26
ruot-nyak <nyak.g.ruot@gmail.com>
2020-11-19 21:30:18 -0800

    added get_balance function, and tested

--- bank-account.py
+++ bank-account.py
@@ -2,11 +2,11 @@ from random import randint
 
 #defining the bankaccount class to put attributes and functions in
 class BankAccount:
-    routing_number = 123456789
     def __init__ (self, name):
         self.name = name
         self.account_number = randint(10000000, 99999999)
         self.balance = 0
+        self.routing_number = 123456789
 
     def deposit (self,amount):
         #+= automatically adds the inputed amount to our total balance
@@ -21,14 +21,24 @@ class BankAccount:
             self.balance -= 10
             print(f'Insufficient funds')
 
+    def get_balance (self):
+        print(f'You have the balance of: ${self.balance}.')
+        return self.balance
+
+        
+
 
 #Testing Init code
 my_acc = BankAccount('Ruot')
 print(my_acc.name)
 print(my_acc.account_number)
 print(my_acc.balance)
+print(my_acc.routing_number)
 #-----------------------------
 #Testing deposit and withdraw function
 my_acc.deposit(20)
 my_acc.withdraw(21)
 print(my_acc.balance)
+#-----------------------------
+#testing get_balance
+my_acc.get_balance()
